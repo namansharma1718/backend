2e857dde3f10f1f15ab6688c831914d9d6970d94
namansharma1718 <48176249+namansharma1718@users.noreply.github.com>
2022-06-10 19:52:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http=require('http');
 const app=require('./app');
+const hostname='0.0.0.0'
 
 const server=http.createServer(app);
-server.listen(process.env.Port || 5000);
+server.listen(process.env.Port || 5000, hostname);
 
 
     
